b4788255b2cf81468aa7564f467b563ef1b429f0
Anik1998 <kunduaniket1998@gmail.com>
2020-11-11 22:17:52 +0530

    logindto

--- src/main/java/com/lti/dto/LoginDto.java
+++ src/main/java/com/lti/dto/LoginDto.java
@@ -7,7 +7,12 @@ public class LoginDto {
 	public String getEmail() {
 		return email;
 	}
-	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
 	public void setPassword(String password) {
 		this.password = password;
 	}
